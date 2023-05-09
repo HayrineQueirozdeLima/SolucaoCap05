d7f974ff894ad580be36d57e26bdedd643b8c3d7
Hayrine Lima <79496093+HayrineQueirozdeLima@users.noreply.github.com>
2023-05-08 20:21:31 -0400

    Adicionar arquivos de projeto.

---
+++ ADO_NETProject01/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace ADO_NETProject01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.labelCNPJ = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxCNPJ = new System.Windows.Forms.TextBox();
+            this.buttonGravar = new System.Windows.Forms.Button();
+            this.buttonNovo = new System.Windows.Forms.Button();
+            this.buttonRemover = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(12, 33);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(21, 17);
+            this.labelID.TabIndex = 0;
+            this.labelID.Text = "ID";
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(12, 82);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(45, 17);
+            this.labelNome.TabIndex = 1;
+            this.labelNome.Text = "Nome";
+            // 
+            // labelCNPJ
+            // 
+            this.labelCNPJ.AutoSize = true;
+            this.labelCNPJ.Location = new System.Drawing.Point(12, 134);
+            this.labelCNPJ.Name = "labelCNPJ";
+            this.labelCNPJ.Size = new System.Drawing.Size(43, 17);
+            this.labelCNPJ.TabIndex = 2;
+            this.labelCNPJ.Text = "CNPJ";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(96, 33);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(302, 22);
+            this.textBoxID.TabIndex = 3;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(96, 77);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(302, 22);
+            this.textBoxNome.TabIndex = 4;
+            // 
+            // textBoxCNPJ
+            // 
+            this.textBoxCNPJ.Location = new System.Drawing.Point(96, 134);
+            this.textBoxCNPJ.Name = "textBoxCNPJ";
+            this.textBoxCNPJ.Size = new System.Drawing.Size(302, 22);
+            this.textBoxCNPJ.TabIndex = 5;
+            // 
+            // buttonGravar
+            // 
+            this.buttonGravar.Location = new System.Drawing.Point(121, 194);
+            this.buttonGravar.Name = "buttonGravar";
+            this.buttonGravar.Size = new System.Drawing.Size(75, 23);
+            this.buttonGravar.TabIndex = 6;
+            this.buttonGravar.Text = "Gravar";
+            this.buttonGravar.UseVisualStyleBackColor = true;
+            this.buttonGravar.Click += new System.EventHandler(this.buttonGravar_Click);
+            // 
+            // buttonNovo
+            // 
+            this.buttonNovo.Location = new System.Drawing.Point(15, 194);
+            this.buttonNovo.Name = "buttonNovo";
+            this.buttonNovo.Size = new System.Drawing.Size(75, 23);
+            this.buttonNovo.TabIndex = 7;
+            this.buttonNovo.Text = "Novo";
+            this.buttonNovo.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemover
+            // 
+            this.buttonRemover.Location = new System.Drawing.Point(225, 194);
+            this.buttonRemover.Name = "buttonRemover";
+            this.buttonRemover.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemover.TabIndex = 8;
+            this.buttonRemover.Text = "Remover";
+            this.buttonRemover.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 238);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(383, 200);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(323, 194);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelar.TabIndex = 10;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 450);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonRemover);
+            this.Controls.Add(this.buttonNovo);
+            this.Controls.Add(this.buttonGravar);
+            this.Controls.Add(this.textBoxCNPJ);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.labelCNPJ);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.labelID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelNome;
+        private System.Windows.Forms.Label labelCNPJ;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox textBoxCNPJ;
+        private System.Windows.Forms.Button buttonGravar;
+        private System.Windows.Forms.Button buttonNovo;
+        private System.Windows.Forms.Button buttonRemover;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonCancelar;
+    }
+}
+
